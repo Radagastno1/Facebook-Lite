78e509762cb05ad2e039e56c4c4e3a960566554c
Angelina Holmqvist <angelinaholmqvist@live.se>
2023-01-01 15:20:27 +0100

    added query in statisticsdb to get average age of users

--- DATABASE/StatisticDB.cs
+++ DATABASE/StatisticDB.cs
@@ -1,7 +1,7 @@
 using Dapper;
 using MySqlConnector;
 namespace DATABASE;
-public class StatisticDB
+public class StatisticDB  //används inte i programmet än, men experimenterar med statistik
 {
     public float GetPercentageActiveUsers()
     {
@@ -13,13 +13,21 @@ public class StatisticDB
     }
     public Dictionary<string, float> GetPercentageAudience()
     {
-        string query = "SELECT gender, COUNT(*) * 100/ " + 
-                    "(SELECT COUNT (*) FROM users) " +      
+        string query = "SELECT gender, COUNT(*) * 100/ " +
+                    "(SELECT COUNT (*) FROM users) " +
                     "FROM users " +
                     "GROUP BY gender;";
         using MySqlConnection con = new MySqlConnection($"Server=localhost;Database=facebook_lite;Uid=root;Pwd=;");
-        Dictionary<string, float> audiencePercentage  = con.Query<KeyValuePair<string, float>>(query).ToDictionary(pair => pair.Key, pair => pair.Value);
+        Dictionary<string, float> audiencePercentage = con.Query<KeyValuePair<string, float>>(query).ToDictionary(pair => pair.Key, pair => pair.Value);
         return audiencePercentage;
     }
-  
+    public float GetAverageAge()
+    {
+        string query = "SELECT AVG(2022-YEAR(birth_date)) " +
+                        "FROM users;";
+        using MySqlConnection con = new MySqlConnection($"Server=localhost;Database=facebook_lite;Uid=root;Pwd=;");
+        float averageAge = con.QuerySingle<float>(query);
+        return averageAge;
+    }
+
 }
