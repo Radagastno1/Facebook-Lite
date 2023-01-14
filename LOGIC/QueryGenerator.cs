7db4a68bb9d8843a0fa00ec93a5b5f6ca3214974
Angelina Holmqvist <angelinaholmqvist@live.se>
2023-01-14 11:27:53 +0100

    added generic data class and a class - querygenerator. so to handle CRUD querys we only need these two classes and the interface IData.

--- LOGIC/QueryGenerator.cs
+++ LOGIC/QueryGenerator.cs
@@ -13,12 +13,20 @@ public class QueryGenerator<T>
        "SELECT LAST_INSERT_ID();";
                 break;
             case Message:
+        query = "INSERT INTO messages (content, sender_id, conversations_id) " +
+        "VALUES(@Content, @SenderId, @ConversationId);";
                 break;
             case Conversation:
+           query =  "INSERT INTO conversations(creator_id) VALUES(@CreatorId);" +
+        "SELECT LAST_INSERT_ID();";
                 break;
             case Comment:
+           query = "INSERT INTO posts (content, users_id, posts_types_id, on_post_id) " +
+         "VALUES (@Content, @UserId, 2, @OnPostId);";
                 break;
             case Post:
+            query = "INSERT INTO posts (content, users_id, posts_types_id) " +
+        "VALUES(@Content, @UserId, 1); SELECT LAST_INSERT_ID()";
                 break;
         }
         return query;
@@ -37,10 +45,32 @@ public class QueryGenerator<T>
             case Message:
                 break;
             case Conversation:
+            query = "SELECT uc.conversations_id as 'Id', c.date_created as 'DateCreated', c.creator_id as 'CreatorId', u.id as 'ParticipantId'" +
+                        "FROM conversations c " +
+                        "INNER JOIN users_conversations uc " +
+                        "ON c.id = uc.conversations_id " +
+                        "INNER JOIN users u " +
+                         "ON u.id = uc.users_id " +
+                         "WHERE u.is_active = true " +
+                       "AND u.id = @ID;";
                 break;
             case Comment:
+            query = "SELECT p.id, p.content, p.date_created as 'DateCreated', p.users_id as 'UserId', " +
+         "p.on_post_id as 'OnPostId', u.first_name as 'FirstName', u.last_name as 'LastName' FROM posts p " +
+         "INNER JOIN users u ON p.users_id = u.id " +
+         "WHERE p.posts_types_id = 2 AND p.is_visible = TRUE AND p.users_id = @userId;";
                 break;
             case Post:
+            query = "SELECT p.id as 'Id', p.content as 'Content', p.date_created as 'DateCreated', " +
+        "u.first_name as 'FirstName', u.last_name as 'LastName', p.users_id as 'UserId' " +
+        "FROM posts p INNER JOIN users u " +
+        "ON p.users_id = u.id " +
+        "INNER JOIN users_friends uf1 " +
+        "ON u.id = uf1.users_id1 " +
+        "WHERE p.posts_types_id = 1 " +
+        "AND p.is_visible = TRUE " +
+        "AND p.is_deleted = FALSE " +
+        "AND uf1.users_id2 = @Id;";
                 break;
         }
         return query;
@@ -56,12 +86,17 @@ public class QueryGenerator<T>
          "about_me = @AboutMe WHERE id = @ID;";
                 break;
             case Message:
+             query = "UPDATE messages SET content = @Content WHERE id = @Id;";
                 break;
             case Conversation:
+            query = "INSERT INTO users_conversations(users_id, conversations_id) VALUES (@participantId, @Id);" +
+        "SELECT LAST_INSERT_ID();";
                 break;
             case Comment:
+            query = "UPDATE posts SET content = @Content WHERE id = @Id;";
                 break;
             case Post:
+            query = "UPDATE posts SET content = @Content, is_edited = True WHERE id = @Id;";
                 break;
         }
         return query;
@@ -79,12 +114,16 @@ public class QueryGenerator<T>
          "COMMIT;";
                 break;
             case Message:
+            query = "UPDATE messages m SET m.is_visible = false " +
+    "WHERE id = @Id;";
                 break;
             case Conversation:
                 break;
             case Comment:
+            query = "UPDATE posts SET is_visible = FALSE WHERE id = @Id AND on_post_id = @OnPostId;";
                 break;
             case Post:
+            query = "Update posts SET is_deleted = TRUE WHERE id = @Id;";
                 break;
         }
         return query;
